b0bc3f78caf5f2ec3954896108a40f128d4da836
Urjit Chakraborty <135136842+urjit-chakraborty@users.noreply.github.com>
2025-09-12 20:39:25 -0400

    fix cors

--- api/main.py
+++ api/main.py
@@ -41,7 +41,7 @@ app = FastAPI(
     version="1.0.0"
 )
 
-origins = ["http://localhost:4321", "null", "http://localhost:5173", "http://127.0.0.1:5173"]
+origins = ["http://localhost:4321", "null", "http://localhost:5173", "http://127.0.0.1:5173", "https://thinkex.netlify.app"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
